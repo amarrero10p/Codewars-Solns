ef53e21a543dbcb589eb38f484d9b1c7561976ce
amarrero10p <alejandromarrero89@gmail.com>
2023-02-06 22:56:51 -0400

    initial commit

---
+++ helloproject.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "\n" << "This is a simple repository with some solutions\n" << "for Codewars problems.\n";
+    cout << "Languages: \n" << "1. Python\n" << "2.C++\n";
+
+}
